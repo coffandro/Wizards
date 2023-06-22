df9d998b2fa5dad48d6eebc26dedf3af39d2c035
Cornelius Andreas Rosenaa <75324436+coffandro@users.noreply.github.com>
2023-06-22 22:23:23 +0200

    added the player to this

---
+++ Player.gd
@@ -0,0 +1,47 @@
+extends KinematicBody
+
+export var PlayerSpeed = 8
+export var SpeedBoost = 1
+
+export var fall_acceleration = 75
+export var jump_impulse = 20
+
+export (PackedScene) var Bullet
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	if Input.is_action_pressed("sprint"):
+		SpeedBoost = 2
+		$Pivot/RunningParticles.set_emitting(true)
+	else:
+		SpeedBoost = 1
+		$Pivot/RunningParticles.set_emitting(false)
+	
+	if Input.is_action_just_pressed("attack") and $CooldownTimer.is_stopped() and not Input.is_action_pressed("sprint"):
+		print("attack")
+		var b = Bullet.instance()
+		owner.add_child(b)
+		b.transform = $Pivot/Shoot.global_transform
+		b.velocity = -b.transform.basis.z * b.muzzle_velocity
+		$CooldownTimer.start()
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+	
+	velocity.x = direction.x * PlayerSpeed * SpeedBoost
+	velocity.z = direction.z * PlayerSpeed * SpeedBoost
+	velocity.y -= fall_acceleration * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
