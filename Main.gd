65d43bb189264fedc16e2a652aab1adedeba38a5
Cornelius Andreas Rosenaa <75324436+coffandro@users.noreply.github.com>
2023-10-11 16:10:25 +0200

    Added the old wizards version

--- Main.gd
+++ Main.gd
@@ -1,12 +1,15 @@
 extends Node
 
-export (PackedScene) var enemy_scene
-
-var EnemyNumber = 3
-
 func _ready():
-	for i in EnemyNumber:
-		randomize()
-		var enemy = enemy_scene.instance()
-		enemy.transform.origin = Vector3(rand_range(-7, 7), 1, rand_range(-7, 7))
-		add_child(enemy)
+	$UserInterface/SettingsBackground.hide()
+
+# Buttons
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+func _on_SettingsButton_pressed():
+	$UserInterface/SettingsBackground.show()
+
+
+func _on_ReturnButton_pressed():
+	$UserInterface/SettingsBackground.hide()
